b4d190a247f81de23496f27891afd6fd192a9d59
shivani283 <shivanivikhar283@gmail.com>
2023-09-09 17:05:51 +0530

    accordian, api, common and slider.json added

---
+++ client/src/utils/accordion.jsx
@@ -0,0 +1,24 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck />,
+    heading: "Secure Investment Opportunities",
+    detail:
+      "Discover the best interest rates on the market while ensuring the safety of your investments. Our platform rigorously vets startups and ventures, offering you a range of secure opportunities to grow your portfolio.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Minimize Risk, Maximize Rewards",
+    detail:
+      "Say goodbye to unstable prices. Our curated selection of investment campaigns is designed to prevent volatility, helping you make informed decisions and safeguard your financial interests.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Optimized Investment Value",
+    detail:
+      "Achieve the best price on the market through our platform. We analyze each campaign rigorously, ensuring that you get the most out of your investments and capitalize on promising opportunities.",
+  },
+];
+
+export default data;
